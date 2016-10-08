aaca7d378b4e33682d680bba6c9cf5662e3798b9
Adam Bell <agbell@gmail.com>
2016-10-08 17:45:30 +0000

    One elevator solution

--- src/main/scala/com/cascadeofinsights/skeleton/ElevatorState.scala
+++ src/main/scala/com/cascadeofinsights/skeleton/ElevatorState.scala
@@ -2,6 +2,8 @@ package com.cascadeofinsights.skeleton
 
 import com.cascadeofinsights.skeleton.Direction.Direction
 
+import scala.collection.Set
+
 case class ElevatorState(
     currentFloor: Int = 1,
     pendingPickups: List[Pickup] = List.empty,
@@ -13,6 +15,23 @@ case class ElevatorState(
   }
 
   def step(): ElevatorState = {
-    null
+    def remainingPickups(floor: Int): List[Pickup] = pendingPickups.filter(_.currentFloor != floor)
+    def joiningPickups(floor: Int): List[Pickup] = pendingPickups.filter(_.currentFloor == floor)
+    def newDropOffs(floor: Int): List[Int] = joiningPickups(floor).map(_.goalFloor)
+    def remainingOriginalDropsOffs(floor: Int): Set[Int] = pendingDropOffs - floor
+    def dropOffs(floor: Int): Set[Int] = (remainingOriginalDropsOffs(floor).toList ++ newDropOffs(floor)).toSet
+
+    //If we have nothing to do, then we are done
+    if (pendingDropOffs.isEmpty && pendingPickups.isEmpty) {
+      this
+    } else {
+      //Increment floor
+      val floor = direction match {
+        case Direction.Up => currentFloor + 1
+        case _ => currentFloor - 1
+      }
+      ElevatorState(currentFloor = floor, pendingPickups = remainingPickups(currentFloor), pendingDropOffs = dropOffs(currentFloor), direction)
+    }
+
   }
 }
